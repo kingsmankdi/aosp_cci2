60ba9ce6b2be01eb04dbc7314a624b896e9f7afa
dustxyz <dustxyz@criptext.com>
2021-04-16 06:08:36 -0400

    update

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -27,6 +27,8 @@ sudo apt install \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
     libxml-simple-perl apt-utils gh \
 
+apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python libncurses5
+
 git clone https://github.com/dustxyz/scripts
 cd scripts
 chmod +x setup/android_build_env.sh
