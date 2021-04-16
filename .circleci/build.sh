42cb88687c48d8baccc8bb4badfb2aff02971dcb
dustxyz <dustxyz@criptext.com>
2021-04-16 04:09:58 -0400

    moved sync to .sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,6 +6,10 @@ PATH="${HOME}/.bin:${PATH}"
 curl https://storage.googleapis.com/git-repo-downloads/repo > .bin/repo
 chmod a+rx .bin/repo
 
+echo "Repo sync"
+repo init -u https://github.com/HyconOS/manifest -b eleven
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/Hycon-Devices/device_xiaomi_ysl device/xiaomi/ysl
 git clone --depth=1 https://github.com/Hycon-Devices/vendor_xiaomi_ysl vendor/xiaomi/ysl
