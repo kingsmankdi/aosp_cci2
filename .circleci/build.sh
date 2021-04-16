6fc6627ba09b715f7edfba282d18ed13cc3cda46
dustxyz <dustxyz@criptext.com>
2021-04-16 04:06:09 -0400

    added repo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+echo "Installing Google Repo"
+mkdir -p .bin
+PATH="${HOME}/.bin:${PATH}"
+curl https://storage.googleapis.com/git-repo-downloads/repo > .bin/repo
+chmod a+rx .bin/repo
+
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/Hycon-Devices/device_xiaomi_ysl device/xiaomi/ysl
 git clone --depth=1 https://github.com/Hycon-Devices/vendor_xiaomi_ysl vendor/xiaomi/ysl
