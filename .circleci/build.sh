69f70deff5d294bb1a865b0c6fc9a3565b2b1d16
dustxyz <dustxyz@criptext.com>
2021-04-16 05:30:54 -0400

    added hals

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -26,6 +26,9 @@ chmod +x setup/android_build_env.sh
 bash setup/android_build_env.sh
 cd ..
 
+echo " -----------Cloning Hals--------------"
+rm -rf vendor/codeaurora/telephony hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone https://github.com/wave-project/vendor_codeaurora_telephony --depth=1 --single-branch vendor/codeaurora/telephony/ && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+
 echo "Repo sync"
 repo init -u https://github.com/HyconOS/manifest -b eleven
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
