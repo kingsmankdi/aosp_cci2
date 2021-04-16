a1c2ca114367aaa3d4c945740b1cbd56dd12bc04
dustxyz <dustxyz@criptext.com>
2021-04-16 04:47:15 -0400

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+
+mkdir hycon
+cd hycon
+
+echo "Installing repo"
+sudo apt-get install repo
+
+echo "Installing scripts"
+git clone https://github.com/akhilnarang/scripts
+cd scripts 
+chmod +x setup/android_build_env.sh
+./setup/android_build_env.sh
+cd ..
+
+echo -e "sync"
+repo init -u https://github.com/HyconOS/manifest -b eleven
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/Hycon-Devices/device_xiaomi_ysl device/xiaomi/ysl
 git clone --depth=1 https://github.com/Hycon-Devices/vendor_xiaomi_ysl vendor/xiaomi/ysl
@@ -8,4 +26,4 @@ echo "Done"
 
 . build/envsetup.sh
 lunch aosp_ysl-userdebug
-mka bacon -j8
+mka bacon -j9
