02d17782c1408e25c7b13baf07eeedd0d59c1add
dustxyz <dustxyz@criptext.com>
2021-04-18 14:04:12 -0400

    update

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -27,7 +27,19 @@ sudo apt install -y \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
     libxml-simple-perl apt-utils \
 
-apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python libncurses5
+apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python libncurses5 python-lunch libwxgtk2.8-dev
+
+echo -e "----- Enabling ccache-----"
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+ccache -o compression=true
+
+echo -e "----- Enabling ccache-----"
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+ccache -o compression=true
 
 
 echo "-------------- Installing scripts----------------"
@@ -53,5 +65,5 @@ echo "Done"
 
 echo -e "Building"
 bash build/envsetup.sh
-echo " BUILD/ENVSETUP.SH CALLED"
-lunch aosp_ysl-userdebug & mka bacon -j$(nproc --all)
+echo "BUILD/ENVSETUP.SH CALLED"
+lunch aosp_ysl-userdebug & make -j$(nproc --all) bacon
