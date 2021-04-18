bf4149666cbf872ab479e59fa12b97b9139b95ff
dustxyz <dustxyz@criptext.com>
2021-04-18 13:11:36 -0400

    update

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -15,7 +15,7 @@ chmod a+rx ~/.bin/repo
 
 
 echo -e "Installing tools"
-sudo apt install \
+sudo apt install -y \
     adb autoconf automake axel bc bison build-essential \
     ccache clang cmake expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git gnupg gperf \
@@ -25,13 +25,13 @@ sudo apt install \
     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
     pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
-    libxml-simple-perl apt-utils gh \
+    libxml-simple-perl apt-utils \
 
 apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python libncurses5
 
 
 echo "-------------- Installing scripts----------------"
-git clone https://github.com/akhilnarang/scripts
+git clone https://github.com/dustxyz/scripts
 cd scripts
 bash setup/android_build_env.sh
 cd ..
