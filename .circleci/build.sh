d941591d2a9121c6f4bb532e16894864a910f8c7
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-19 11:19:32 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,8 +5,8 @@ apt install sudo
 echo -e "number of cores?"
 nproc --all
 
-mkdir hycon 
-cd hycon
+mkdir pe
+cd pe
 echo -e "Installing Google Repo"
 mkdir -p ~/.bin
 PATH="${HOME}/.bin:${PATH}"
@@ -43,27 +43,27 @@ ccache -o compression=true
 
 
 echo "-------------- Installing scripts----------------"
-git clone https://github.com/dustxyz/scripts
-cd scripts
+git clone https://github.com/flaahokiller/scripts-1
+cd scripts-1
 bash setup/android_build_env.sh
 cd ..
 
 
 echo "----------------Repo Sync----------------"
-repo init -u https://github.com/HyconOS/manifest.git -b eleven
+repo init -u https://github.com/PixelExperience/manifest -b eleven
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 echo " -----------Cloning Hals--------------"
 rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
 
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/Hycon-Devices/device_xiaomi_ysl device/xiaomi/ysl
-git clone --depth=1 https://github.com/Hycon-Devices/vendor_xiaomi_ysl vendor/xiaomi/ysl
-git clone --depth=1 https://github.com/Hycon-Devices/kernel_xiaomi_ysl kernel/xiaomi/ysl
+git clone --depth=1 https://github.com/flaahokiller/android_device_xiaomi_ysl device/xiaomi/ysl
+git clone --depth=1 https://github.com/ItsVixano/android_vendor_xiaomi_ysl vendor/xiaomi/ysl
+git clone --depth=1 https://github.com/flaahokiller/flasho_Ysl kernel/xiaomi/ysl
 
 echo "Done"
-
+nproc
 echo -e "Building"
 bash build/envsetup.sh
 echo "BUILD/ENVSETUP.SH CALLED"
-lunch aosp_ysl-userdebug & make -j$(nproc --all) bacon
+lunch aosp_ysl-user & make -j$(nproc --all) bacon
